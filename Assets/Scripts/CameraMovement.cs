8674797471e2b5934de5efd4ef99d8e4326bd9c8
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-01 10:52:56 +0100

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float fastMoveFactor;
+    [SerializeField] private float climbSpeed;
+    [SerializeField] private float cameraSensitivity;
+
+    private float _rotationX;
+    private float _rotationY;
+
+    void Start()
+    {
+        _rotationX = transform.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            ToggleCursor(false);
+            _rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+            _rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+            _rotationY = Mathf.Clamp(_rotationY, -90, 90);
+        }
+        if (Input.GetMouseButtonUp(1)) { ToggleCursor(true); }
+
+        Quaternion targetRotation = Quaternion.AngleAxis(_rotationX, Vector3.up);
+        targetRotation *= Quaternion.AngleAxis(_rotationY, Vector3.left);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 4f);
+
+        float speedFactor = 1f;
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) speedFactor = fastMoveFactor;
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) speedFactor = fastMoveFactor;
+
+        transform.position += transform.forward * movementSpeed * speedFactor * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += transform.right * movementSpeed * speedFactor * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float upAxis = 0;
+        if (Input.GetKey(KeyCode.Q)) upAxis = -0.5f;
+        if (Input.GetKey(KeyCode.E)) upAxis = 0.5f;
+        transform.position += transform.up * movementSpeed * speedFactor * upAxis * Time.deltaTime;
+    }
+
+
+    private void ToggleCursor(bool value)
+    {
+        if (value) { Cursor.lockState = CursorLockMode.None; } else { Cursor.lockState = CursorLockMode.Locked; }
+        Cursor.visible = value;
+    }
+}
