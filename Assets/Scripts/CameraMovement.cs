55fc239ced9d801cc9b2b47ef2c8d79ea6fd48a0
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-05 15:43:09 +0100

    Added new camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,36 +2,99 @@
 
 public class CameraMovement : MonoBehaviour
 {
-    [Header("Camera Settings")]
-    [SerializeField] private float cameraMovementSpeed;
-    [SerializeField] private float rotationSpeed;
 
-    [Header("World Settings")]
-    [SerializeField] private float waterheight;
-    [SerializeField] private float worldBottomLevel;
+    [SerializeField] private float panSpeed = 20f;
+    [SerializeField] private float rotSpeed = 10f;
+    [SerializeField] private float zoomSpeed = 50f;
+    [SerializeField] private float borderWidth = 10f;
+    [SerializeField] private bool edgeScrolling = true;
+    [SerializeField] private float zoomMin = 0.0f;
+    [SerializeField] private float zoomMax = 49.0f;
+    private float mouseX, mouseY;
+
 
     void Update()
     {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * cameraMovementSpeed * Time.deltaTime);
+        Movement();
+        Rotation();
+        Zoom();
+    }
 
-        if (Input.GetKey(KeyCode.Q)) { transform.Rotate(-transform.up * rotationSpeed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.E)) { transform.Rotate(transform.up * rotationSpeed * Time.deltaTime); }
+
+    void Movement()
+    {
+        Vector3 pos = transform.position;
+        Vector3 forward = transform.forward;
+        forward.y = 0;
+
+        forward.Normalize();
+        Vector3 right = transform.right;
+        right.y = 0;
+        right.Normalize();
+
+        if (Input.GetKey("w") || edgeScrolling == true && Input.mousePosition.y >= Screen.height - borderWidth)
+        {
+            pos += forward * panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || edgeScrolling == true && Input.mousePosition.y <= borderWidth)
+        {
+            pos -= forward * panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || edgeScrolling == true && Input.mousePosition.x >= Screen.width - borderWidth)
+        {
+            pos += right * panSpeed * Time.deltaTime;           
+        }
+
+        if (Input.GetKey("a") || edgeScrolling == true && Input.mousePosition.x <= borderWidth)
+        {
+            pos -= right * panSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+
+
+    void Rotation()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            mouseX += Input.GetAxis("Mouse X") * rotSpeed;
+            mouseY -= Input.GetAxis("Mouse Y") * rotSpeed;
+            mouseY = Mathf.Clamp(mouseY, -30, 45);
+
+            ToggleCursor(false);
+            transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else
+        {
+            ToggleCursor(true);
+        }
+    }
+
+
+    void Zoom()
+    {
+        Vector3 camPos = transform.position;
+
+        float distance = Vector3.Distance(transform.position, new Vector3(transform.position.x, 0, transform.position.z));
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && distance > zoomMin)
+        {
+            camPos += transform.forward * zoomSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f && distance < zoomMax)
+        {
+            camPos -= transform.forward * zoomSpeed * Time.deltaTime;
+        }
+        transform.position = camPos;
     }
 
     private void ToggleCursor(bool value)
     {
-        //if (value) { Cursor.lockState = CursorLockMode.None; } else { Cursor.lockState = CursorLockMode.Locked; }
+        if (value) { Cursor.lockState = CursorLockMode.None; } else { Cursor.lockState = CursorLockMode.Locked; }
         Cursor.visible = value;
     }
-
 }
-
-
-/*      //If the player is underwater then enable the underwater fog
-  if (transform.position.y <= waterheight) { RenderSettings.fog = true; }
-  else
-  {
-      RenderSettings.fog = false;
-      if (transform.position.y <= worldBottomLevel) { transform.position = new Vector3(transform.position.x, spawnHeight, transform.position.z); }
-  }*/
 
