f840d6f666d8faec67b8d4f5b496c9706e958d7e
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-05 15:13:24 +0100

    Making new camera solution

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,65 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     [Header("Camera Settings")]
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float fastMoveFactor;
-    [SerializeField] private float climbSpeed;
-    [SerializeField] private float cameraSensitivity;
+    [SerializeField] private float cameraMovementSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    [Header("World Settings")]
     [SerializeField] private float waterheight;
     [SerializeField] private float worldBottomLevel;
 
-    private float spawnHeight;
-    private float _rotationX;
-    private float _rotationY;
-
-    void Start()
-    {
-        _rotationX = transform.eulerAngles.y;
-        spawnHeight = transform.position.y;
-    }
-
     void Update()
     {
-        if (Input.GetMouseButton(1))
-        {
-            ToggleCursor(false);
-            _rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-            _rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-            _rotationY = Mathf.Clamp(_rotationY, -90, 90);
-        }
-        if (Input.GetMouseButtonUp(1)) { ToggleCursor(true); }
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * cameraMovementSpeed * Time.deltaTime);
 
-        Quaternion targetRotation = Quaternion.AngleAxis(_rotationX, Vector3.up);
-        targetRotation *= Quaternion.AngleAxis(_rotationY, Vector3.left);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 4f);
-
-        float speedFactor = 1f;
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) speedFactor = fastMoveFactor;
-        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) speedFactor = fastMoveFactor;
-
-        transform.position += transform.forward * movementSpeed * speedFactor * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position += transform.right * movementSpeed * speedFactor * Input.GetAxis("Horizontal") * Time.deltaTime;
-        float upAxis = 0;
-        if (Input.GetKey(KeyCode.Q)) upAxis = -0.5f;
-        if (Input.GetKey(KeyCode.E)) upAxis = 0.5f;
-        transform.position += transform.up * movementSpeed * speedFactor * upAxis * Time.deltaTime;
-
-        //If the player is underwater then enable the underwater fog
-        if (transform.position.y <= waterheight) { RenderSettings.fog = true; } else
-        {
-            RenderSettings.fog = false;
-            if (transform.position.y <= worldBottomLevel) { transform.position = new Vector3(transform.position.x, spawnHeight, transform.position.z); }
-        }
+        if (Input.GetKey(KeyCode.Q)) { transform.Rotate(-transform.up * rotationSpeed * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.E)) { transform.Rotate(transform.up * rotationSpeed * Time.deltaTime); }
     }
-
 
     private void ToggleCursor(bool value)
     {
-        if (value) { Cursor.lockState = CursorLockMode.None; } else { Cursor.lockState = CursorLockMode.Locked; }
+        //if (value) { Cursor.lockState = CursorLockMode.None; } else { Cursor.lockState = CursorLockMode.Locked; }
         Cursor.visible = value;
     }
+
 }
+
+
+/*      //If the player is underwater then enable the underwater fog
+  if (transform.position.y <= waterheight) { RenderSettings.fog = true; }
+  else
+  {
+      RenderSettings.fog = false;
+      if (transform.position.y <= worldBottomLevel) { transform.position = new Vector3(transform.position.x, spawnHeight, transform.position.z); }
+  }*/
+
