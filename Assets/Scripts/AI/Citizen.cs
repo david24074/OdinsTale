d011ff45fd000cc45922be9d9b4652d266d67399
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-08 09:53:50 +0100

    Created citizen script

---
+++ Assets/Scripts/AI/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Citizen : MonoBehaviour
+{
+    private NavMeshAgent navmeshAgent;
+
+    private void Start()
+    {
+        navmeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetTransformTarget(Transform target)
+    {
+        navmeshAgent.SetDestination(target.position);
+    }
+}
