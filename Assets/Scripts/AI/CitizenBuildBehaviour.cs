56f73dec0ddf434044659f4555630d2447e1038b
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-10 16:04:38 +0100

    Started on building buildings

---
+++ Assets/Scripts/AI/CitizenBuildBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenBuildBehaviour : StateMachineBehaviour
+{
+    private ConstructionBuilding targetBuilding;
+    private float mineInterval = 1, currentTimer;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        targetBuilding = animator.GetComponent<Citizen>().GetCurrentTarget().GetComponent<ConstructionBuilding>();
+        currentTimer = mineInterval;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!targetBuilding)
+        {
+            animator.GetComponent<Citizen>().QuitJob();
+        }
+
+        currentTimer -= 1 * Time.deltaTime;
+
+        if (currentTimer <= 0)
+        {
+            currentTimer = mineInterval;
+            targetBuilding.BuildObject(20);
+        }
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Assets/Scripts/ConstructionBuilding.cs
+++ Assets/Scripts/ConstructionBuilding.cs
@@ -1,9 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ConstructionBuilding : MonoBehaviour
 {
+    //We want to move the mesh object downwards when the building is instantiated
+    [SerializeField] private float moveDownYLevel, buildHealth = 1000;
+    private Transform meshObject;
+    private float amountMoveEachHit;
+
+    private void Start()
+    {
+        meshObject = transform.GetChild(0);
+        meshObject.transform.position = new Vector3(meshObject.position.x, meshObject.position.y + moveDownYLevel, meshObject.position.z);
+        amountMoveEachHit = moveDownYLevel / buildHealth;
+    }
+
+    public void BuildObject(int amount)
+    {
+        transform.DOComplete();
+        transform.DOShakeScale(.5f, .2f, 10, 90, true);
+        buildHealth -= amount;
+        meshObject.transform.localPosition = new Vector3(meshObject.transform.localPosition.x, meshObject.transform.localPosition.y + amountMoveEachHit, meshObject.transform.localPosition.z);
+    }
+
     public GameObject GetBuilding()
     {
         return transform.GetChild(0).gameObject;
