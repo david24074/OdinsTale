1fe0a4c29fb78da6cc94502541920f3aee05ea95
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-12 14:16:40 +0100

    Added particles and random mesh to harvestables

--- Assets/Scripts/Harvestable.cs
+++ Assets/Scripts/Harvestable.cs
@@ -14,12 +14,22 @@ public class Harvestable : MonoBehaviour
     [Header("Resource gain")]
     [SerializeField] private int minAmount = 5;
     [SerializeField] private int maxAmount = 10;
-    
+
+    [Header("Visual")]
+    [SerializeField] private GameObject[] randomMeshPossibilities;
+    [SerializeField] private ParticleSystem damageParticle;
+
+    private void Start()
+    {
+        randomMeshPossibilities[Random.Range(0, randomMeshPossibilities.Length)].SetActive(true);
+    }
+
     public void DoDamageToResource(int damageAmount)
     {
         transform.DOComplete();
         transform.DOShakeScale(.5f, jiggleStrength, 10, 90, true);
         resourceHealth -= damageAmount;
+        Instantiate(damageParticle, transform.position, transform.rotation);
 
         if(resourceHealth <= 0)
         {
