4dcff00f7299b895fc4dcfa3cf7284f62e45c1e2
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-29 11:16:59 +0200

    Made buildings burnable

---
+++ Assets/Scripts/Burnable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burnable : MonoBehaviour
+{
+    [Tooltip("Building health is in seconds")]
+    [SerializeField] private float buildingHealth = 300;
+    private float currentHealth;
+    [SerializeField] private ParticleSystem fireParticles;
+
+    private void Start()
+    {
+        currentHealth = buildingHealth;
+    }
+
+    public void ToggleFire(bool on)
+    {
+        enabled = on;
+        if (on) { fireParticles.Play(); } else { fireParticles.Stop(); currentHealth = buildingHealth; }
+    }
+
+    private void Update()
+    {
+        currentHealth -= 1 * Time.deltaTime;
+
+        if(currentHealth <= 0)
+        {
+
+        }
+    }
+}
