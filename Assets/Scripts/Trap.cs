8f665b4d674bfd181cf58395a4d44b640764ac80
David Meijer <32064704+david24074@users.noreply.github.com>
2021-03-31 13:35:24 +0200

    Added spike trap

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    private Animator animator;
+    private bool trapActivated = false;
+    [SerializeField] private int destroyTimer;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" && !trapActivated)
+        {
+            trapActivated = true;
+            animator.Play("ActivateTrap");
+            Destroy(gameObject, destroyTimer);
+        }
+    }
+}
